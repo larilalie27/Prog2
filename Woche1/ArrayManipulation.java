c714150df2c2c33d4a6377a7b37d596ee2815beb
Larissa Weber <larissa-ruth.weber@informatik.ha-fulda.de>
2022-04-21 15:48:59 +0200

    commit on 'master'

--- Woche1/ArrayManipulation.java
+++ Woche1/ArrayManipulation.java
@@ -13,10 +13,19 @@ public class ArrayManipulation {
          return arr;
 
     }
+
+    public static int[] squareEach(int[] arr) {
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = arr[i] * arr[i];
+        }
+        return arr;
+    }
     public static void main(String[] args) {
        
         int[] arr = {1,2,3,4,5};
         System.out.println(Arrays.toString(reverseArray(arr)));
+        System.out.println(Arrays.toString(squareEach(arr)));
         
     }
 }
